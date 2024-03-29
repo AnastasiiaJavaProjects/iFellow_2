e48a4a548b53525c0c36df8a27a04879e79d7fcc
Filippova Anastasiia <anasty2002@mail.ru>
2024-03-29 23:56:58 +0300

    Функция getApi переведена на cucumber

--- src/main/java/steps/RickAndMortySteps.java
+++ src/main/java/steps/RickAndMortySteps.java
@@ -1,6 +1,10 @@
 package steps;
 
+import io.cucumber.java.ru.Когда;
+import io.cucumber.java.ru.Также;
+import io.cucumber.java.ru.Тогда;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
 
 import java.util.List;
 
@@ -9,34 +13,72 @@ import static util.TestProperties.getProperty;
 
 public class RickAndMortySteps {
 
-    public static String getCharacterId(String name){
+    private String mortysId;
+    private String mortysUrl;
+    private String mortysLocation;
+    private String mortysSpecies;
+    private String mortysLastEpisodeUrl;
+    private String lastCharactersUrl;
+    private String lastCharactersLocation;
+    private String lastCharactersSpecies;
+
+
+    @Когда("Отправляем GET запрос с именем {string} и получаем его id")
+    public void getCharacterId(String name){
         Response response = getApi(getProperty("character.name.filter") + name, 200);
-        return response.body().jsonPath().getString(getProperty("id.path"));
+        mortysId = response.body().jsonPath().getString(getProperty("id.path"));
     }
 
-    public static String getCharacterUrl(String characterId){
-        return getProperty("character.baseurl") + characterId;
+    @Также("Отправляем GET запрос с id Морти и получаем его URL")
+    public void getCharacterUrl(){
+        mortysUrl = getProperty("character.baseurl") + mortysId;
     }
 
-    public static String getCharacterLastEpisodeUrl(String characterId){
-        Response response = getApi(getProperty("character.baseurl") + characterId, 200);
+    @Также("Отправляем GET запрос с URL Морти и получаем данные о местонахождении Морти")
+    public void getMortyLocation(){
+        Response response = getApi(mortysUrl, 200);
+        mortysLocation = response.body().jsonPath().getString(getProperty("location.path"));
+    }
+
+    @Также("Отправляем GET запрос с URL Морти и получаем данные о расе Морти")
+    public void getMortySpecies(){
+        Response response = getApi(mortysUrl, 200);
+        mortysSpecies = response.body().jsonPath().getString(getProperty("species.path"));
+    }
+
+    @Когда("Отправляем GET запрос с id Морти и получаем URL его последнего эпизода")
+    public void getMortysLastEpisodeUrl(){
+        Response response = getApi(getProperty("character.baseurl") + mortysId, 200);
         List<String> episodes = response.body().jsonPath().getList(getProperty("episode.path"));
-        return episodes.get(episodes.size()-1);
+        mortysLastEpisodeUrl = episodes.get(episodes.size()-1);
     }
 
-    public static String getLastCharacterUrl(String episodeUrl){
-        Response response = getApi(episodeUrl, 200);
+    @Также("Отправляем GET запрос с URL последнего эпизода Морти и получаем URL последнего персонажа в этом эпизоде")
+    public void getLastCharacterUrl(){
+        Response response = getApi(mortysLastEpisodeUrl, 200);
         List<String> characters = response.body().jsonPath().getList(getProperty("characters.path"));
-        return characters.get(characters.size()-1);
+        lastCharactersUrl = characters.get(characters.size()-1);
     }
 
-    public static String getCharactersLocation(String characterUrl){
-        Response response = getApi(characterUrl, 200);
-        return response.body().jsonPath().getString(getProperty("location.path"));
+    @Также("Отправляем GET запрос с URL последнего персонажа и получаем данные о его местонахождении")
+    public void getLastCharacterLocation(){
+        Response response = getApi(lastCharactersUrl, 200);
+        lastCharactersLocation = response.body().jsonPath().getString(getProperty("location.path"));
     }
 
-    public static String getCharactersSpecies(String characterUrl){
-        Response response = getApi(characterUrl, 200);
-        return response.body().jsonPath().getString(getProperty("species.path"));
+    @Также("Отправляем GET запрос с URL последнего персонажа и получаем данные о его расе")
+    public void getLastCharacterSpecies(){
+        Response response = getApi(lastCharactersUrl, 200);
+        lastCharactersSpecies = response.body().jsonPath().getString(getProperty("species.path"));
+    }
+
+    @Тогда("Проверяем, что раса Морти и последнего персонажа совпадают")
+    public void verifySpecies(){
+        Assertions.assertEquals(mortysSpecies, lastCharactersSpecies);
+    }
+
+    @Тогда("Проверяем, что местонахождение Морти и последнего персонажа не совпадают")
+    public void verifyLocations(){
+        Assertions.assertNotEquals(mortysLocation, lastCharactersLocation);
     }
 }
--- src/test/java/tests/RickAndMortyTest.java
+++ src/test/java/tests/RickAndMortyTest.java
@@ -1,27 +1,14 @@
 package tests;
 
 import hooks.WebHooks;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-import static steps.RickAndMortySteps.*;
-import static util.TestProperties.getProperty;
-
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features={"classpath:features"},
+        glue={"classpath:steps", "classpath:hooks"},
+        plugin = {"pretty"}
+)
 public class RickAndMortyTest extends WebHooks {
-
-    @Test
-    public void test1(){
-        String mortysId = getCharacterId(getProperty("test.character"));
-        String mortysUrl = getCharacterUrl(mortysId);
-        String mortysLocation = getCharactersLocation(mortysUrl);
-        String mortysSpecies = getCharactersSpecies(mortysUrl);
-
-        String lastCharacterEpisodeUrl = getCharacterLastEpisodeUrl(mortysId);
-        String lastCharacterUrl = getLastCharacterUrl(lastCharacterEpisodeUrl);
-        String lastCharactersLocation = getCharactersLocation(lastCharacterUrl);
-        String lastCharactersSpecies = getCharactersSpecies(lastCharacterUrl);
-
-        Assertions.assertEquals(mortysSpecies, lastCharactersSpecies);
-        Assertions.assertNotEquals(mortysLocation, lastCharactersLocation);
-    }
 }
